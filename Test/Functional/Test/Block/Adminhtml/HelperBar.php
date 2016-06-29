466ed14014f05564846d64b7c842ca8e4642455e
Alessandro <alessandro.zucca@gmail.com>
2016-06-29 14:39:05 +0100

    MAGEP-19: Adding empty Block, Test Case and adding Block to CmsPageIndex
    Extending CmsPageIndex.xml from the Magento_Cms Module in order to add the HelperBarBlock.
    Adding HelperBar Test Case which logs in on Magento Admin using constaint AssertUserSuccessLogin.

---
+++ Test/Functional/Test/Block/Adminhtml/HelperBar.php
@@ -0,0 +1,9 @@
+<?php
+namespace MX\HelperBar\Test\Block\Adminhtml;
+
+use Magento\Mtf\Block\Block;
+
+class HelperBar extends Block
+{
+
+}
---
+++ Test/Functional/Test/TestCase/HelperBarTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace MX\HelperBar\Test\TestCase;
+
+use Magento\Mtf\TestCase\Injectable;
+
+/**
+ * Steps:
+ * 1. Log in to Admin.
+ * 2. Verify the helper bar is displayed.
+ */
+class HelperBarTest extends Injectable
+{
+    public function test()
+    {
+        // Steps
+    }
+}
