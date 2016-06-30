fbe88346966481de6487ca21470699fab984054d
Alessandro <alessandro.zucca@gmail.com>
2016-06-30 10:36:48 +0100

    MAGEP-19: Completing assertations for developer mode
    - Injecting HelperBar block into Dashboard Page via xml
    - Using HelperBar Test Block to check if element are visible or not
    - Using HelperBar Block in Test Case to perform assertation

--- Test/Functional/Test/TestCase/HelperBarTest.php
+++ Test/Functional/Test/TestCase/HelperBarTest.php
@@ -1,17 +1,48 @@
 <?php
 namespace MX\HelperBar\Test\TestCase;
 
+use Magento\Backend\Test\Page\Adminhtml\Dashboard;
 use Magento\Mtf\TestCase\Injectable;
 
 /**
+ * Preconditions:
+ * 1. Log in to Admin Index Page
+ *
  * Steps:
- * 1. Log in to Admin.
- * 2. Verify the helper bar is displayed.
+ * 1. Verify that is displayed.
+ * 2. Verify that is in developer mode
+ * 3. Verify that has correct content
+ * 4. Verify that has close button
  */
 class HelperBarTest extends Injectable
 {
+    /** @var Dashboard */
+    protected $backendDashboard;
+
+    /**
+     * Injecting test data
+     *
+     * @param Dashboard $backendDashboard
+     */
+    public function __inject(Dashboard $backendDashboard)
+    {
+        $this->backendDashboard = $backendDashboard;
+    }
+
+    /**
+     * Run test
+     */
     public function test()
     {
+        // Preconditions
+        $this->backendDashboard->open();
+
         // Steps
+        $helperBar = $this->backendDashboard->getHelperBarBlock();
+
+        $this->assertTrue($helperBar->isVisible());
+        $this->assertTrue($helperBar->isInDeveloperMode());
+        $this->assertTrue($helperBar->hasProductMetadata());
+        $this->assertTrue($helperBar->hasCloseButton());
     }
 }
